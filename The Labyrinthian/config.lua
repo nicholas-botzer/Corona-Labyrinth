277648aac5280ba9ccb5a3c06fd2054fc68bc9e9
WBotzer <wbotzer@gmail.com>
2013-11-04 22:10:44 -0500

    Added a folder with the proper game name
    now all builds will have the game properly titled

---
+++ The Labyrinthian/config.lua
@@ -0,0 +1,30 @@
+application = {
+	content = {
+		width = 450,
+		height = 800, 
+		--scale = "zoomEven",
+		scale = "letterBox",
+		fps = 30,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]    
+}
---
+++ The Labyrinthian/main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "storyboard" module
+local storyboard = require "storyboard"
+
+-- load menu screen
+storyboard.gotoScene( "menu" )
+
